33f4ac89542514c19467636893f7f2010aec07ad
leo <le.mutuku@gmail.com>
2022-09-09 16:40:43 +0300

    m

--- controller/exam/marks/examLines.js
+++ controller/exam/marks/examLines.js
@@ -19,63 +19,60 @@ const initiateMarks = catchAsync(async (req, res, next) => {
       let getUnitID = await Unit.findById({
             _id: unitID
       })
+      if (!getUnitID) {
+            return res.status(400).json({
+                  status: 'failed',
+                  message: 'Could not find a matching class'
+            })
+      }
       let unitCurrent = getUnitID.unitCode
       const studentsUnit = await Students.find({
-            unitCurrent:unitCurrent 
+            unitCurrent: unitCurrent
       })
-      if(!unitCurrent){
+      if (!unitCurrent) {
             return res.status(200).json({
-               status:"failed",
-               message:'we could not retrive students from the class provided'   
+                  status: "failed",
+                  message: 'we could not retrive students from the class provided'
             })
       }
       let setUpExam = await SetUpExam.findOne({
             _id: examID
       })
       let results = []
-     studentsUnit.forEach(async(el)=>{
-       let studentID = el._id
-       let result = {
-            examID:examID, studentID:studentID
-       }
-       results.push(result)
-     })
-    let final = await CaptureMarks.create(results)
-     res.status(200).json({
-      status:'success',
-      result:final.length,
-      data:final
-     })
+      studentsUnit.forEach(async (el) => {
+            let studentID = el._id
+            let result = {
+                  examID: examID, studentID: studentID
+            }
+            results.push(result)
+      })
+      let final = await CaptureMarks.create(results)
+      res.status(200).json({
+            status: 'success',
+            result: final.length,
+            data: final
+      })
 })
-const captureScoreByExamBySubject = catchAsync(async(req, res, next) => {
-      let { _id, subjectObjectID, score, index } = req.body
-      const findExamList = await CaptureMarks.findOne({ _id: _id })
-      if (!findExamList){
+const captureScoreByExamBySubject = catchAsync(async (req, res, next) => {
+      let { data } = req.body
+      let _id = data._id
+      const findExamList = await CaptureMarks.findOne({_id: _id})
+      if (!findExamList) {
             return res.status(400).json({
                   status: 'failed',
                   message: 'we could not retried a matching id for the selected item'
             })
       }
-      const querySelector = JSON.parse(`{
-      "_id": "${_id}","examinableSubjects.${index}._id": "${subjectObjectID}"
-}`)
-      const query = JSON.parse(`{"examinableSubjects.${index}.score": "${score}"}`)
-      let updateSubjectScore = await CaptureMarks.updateOne(querySelector,
-            {
-                  $set: query
-            },
-            {
-                  multi: true
-            })
-      res.status(200).json({
-            status: 'success',
-            result: updateSubjectScore.length,
-            data: updateSubjectScore
+      let result = await CaptureMarks.update(data)
+      res.status(201).json({
+            status:'success',
+            result:result.length,
+            data:result
       })
 })
 const getCapturedMarksByItemEntryID = catchAsync(async (req, res, next) => {
       let entryID = req.params.entryID
-      const examEntries = await CaptureMarks.findOne({_id: entryID })
+      const examEntries = await CaptureMarks.findOne({ _id: entryID })
       if (!examEntries) {
             return res.status(400).json({
                   status: 'failed',
@@ -88,10 +85,10 @@ const getCapturedMarksByItemEntryID = catchAsync(async (req, res, next) => {
             data: examEntries
       })
 })
-const getCapturedMarksByExamID=catchAsync(async (req, res, next) => {
+const getCapturedMarksByExamID = catchAsync(async (req, res, next) => {
       let examID = req.params.examID
-      const examEntries = await CaptureMarks.find({examID:examID})
-      if (!examEntries){
+      const examEntries = await CaptureMarks.find({ examID: examID })
+      if (!examEntries) {
             return res.status(400).json({
                   status: 'failed',
                   message: 'We could find a matching data for your request'
@@ -103,83 +100,83 @@ const getCapturedMarksByExamID=catchAsync(async (req, res, next) => {
             data: examEntries
       })
 })
-const subjectGradesAndComments=catchAsync(async (req, res, next) => {
-      let { score, isKiwaswahili} = req.body
+const subjectGradesAndComments = catchAsync(async (req, res, next) => {
+      let { score, isKiwaswahili } = req.body
       let grade = ""
       let comment = ""
       let subjectTeacher = ""
       let points = 0
-      if(isKiwaswahili){
-      
-     
-      // calculate subject grade
-      if(score >=80 && score <=100){
-            grade = "A"
-            comment = "Excellent"
-            points = 12
-      } 
-      else if(score >=75 && score <= 79.99){
-            grade = "A-"
-            comment = "V.Good"
-            points = 11 
-      }
-      else if(score >=70 && score <=75.99){
-            grade = "B+"
-            comment = "Good"
-            points = 9
-      }
-      else if(score >=65 && score <=69.99){
-            grade = "B+"
-            comment = "Average"
-            points =8
-      }
-      else if(score >=60 && score <= 64.99 ){
-            grade = "B+"
-            comment = "Fair"
-            points =8
-      }
-      else if(score >= 55 && score <= 59.99){
-            grade = "B+"
-            comment = "Fair"
-            points =7
-      }
-      else if(score >= 50 && score <= 54.99){
-            grade = "B+"
-            comment = "Good trial"
-            points =6
-      }
-      else if(score >= 45 && score <= 49.99){
-            grade = "B+"
-            comment = "Work harder"
-            points =5
-      }
-      else if(score >= 40 && score <= 44.99){
-            grade = "B+"
-            comment = "Improve"
-            points =4
-      }
-      else if(score >=35 && score <= 39.99){
-            grade = "B+"
-            comment = "Put effort"
-            points =3
-      }
-      else if (score >= 30 && score <= 34.99){
-            grade = "B+"
-            comment = "Put effort"
-            points =2
-      }
-      else if(score >= 0 && score <= 29.99){
-            grade = "B+"
-            comment = "Avoid E's"
-            points =1
-      }
-      else{
+      if (isKiwaswahili) {
 
-      }
-}
-else{}
 
-console.log(comment)
+            // calculate subject grade
+            if (score >= 80 && score <= 100) {
+                  grade = "A"
+                  comment = "Excellent"
+                  points = 12
+            }
+            else if (score >= 75 && score <= 79.99) {
+                  grade = "A-"
+                  comment = "V.Good"
+                  points = 11
+            }
+            else if (score >= 70 && score <= 75.99) {
+                  grade = "B+"
+                  comment = "Good"
+                  points = 9
+            }
+            else if (score >= 65 && score <= 69.99) {
+                  grade = "B+"
+                  comment = "Average"
+                  points = 8
+            }
+            else if (score >= 60 && score <= 64.99) {
+                  grade = "B+"
+                  comment = "Fair"
+                  points = 8
+            }
+            else if (score >= 55 && score <= 59.99) {
+                  grade = "B+"
+                  comment = "Fair"
+                  points = 7
+            }
+            else if (score >= 50 && score <= 54.99) {
+                  grade = "B+"
+                  comment = "Good trial"
+                  points = 6
+            }
+            else if (score >= 45 && score <= 49.99) {
+                  grade = "B+"
+                  comment = "Work harder"
+                  points = 5
+            }
+            else if (score >= 40 && score <= 44.99) {
+                  grade = "B+"
+                  comment = "Improve"
+                  points = 4
+            }
+            else if (score >= 35 && score <= 39.99) {
+                  grade = "B+"
+                  comment = "Put effort"
+                  points = 3
+            }
+            else if (score >= 30 && score <= 34.99) {
+                  grade = "B+"
+                  comment = "Put effort"
+                  points = 2
+            }
+            else if (score >= 0 && score <= 29.99) {
+                  grade = "B+"
+                  comment = "Avoid E's"
+                  points = 1
+            }
+            else {
+
+            }
+      }
+      else { }
+
+      console.log(comment)
       // add comments
       // update teachers details
 
--- model/exam/examLines.js
+++ model/exam/examLines.js
@@ -2,126 +2,126 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 mongoose.plugin(schema => { schema.options.usePushEach = true });
 const examLineSchema = new Schema({
-  examID:{type: mongoose.Schema.ObjectId,ref: "ExamHeader"},
-  studentID:{type: mongoose.Schema.ObjectId,ref: "Subject"},
-  english:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-kiswahili:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-mathematics:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-biology:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-chemistry:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-physics:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-geography:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-history:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-cre:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-business:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-agriculture:{
-    subjectFullName:{},
-    subjectShortName:{},
-    subjectCode:{},
-    score:{},
-    points:{},
-    grade:{},
-    subjectTeacher:{},
-    comment:{}
-},
-  totalMarks: Number,
-  totalPoints: Number,
-  overalGrade: String,
-  classTecher:{type: mongoose.Schema.ObjectId,ref: "classTeacher"},
-  classTeacherRemarks:{type: mongoose.Schema.ObjectId,ref: "classTeacherRemarks"},
-  principal:{type: mongoose.Schema.ObjectId,ref: "Principal"},
-  principalRemarks:{type: mongoose.Schema.ObjectId,ref: "PrincipalRemarks"},
-  isArchived: { type: Boolean, Default: false },
-  isActive: { type: Boolean, Default: true },
+    examID: { type: mongoose.Schema.ObjectId, ref: "ExamHeader" },
+    studentID: { type: mongoose.Schema.ObjectId, ref: "Subject" },
+    english: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    kiswahili: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    mathematics: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    biology: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    chemistry: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    physics: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    geography: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    history: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    cre: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    business: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    agriculture: {
+        subjectFullName: { type: String },
+        subjectShortName: { type: String },
+        subjectCode: { type: String },
+        score: { type: Number },
+        points: { type: Number },
+        grade: { type: String },
+        subjectTeacher: { type: String },
+        comment: { type: String }
+    },
+    totalMarks: Number,
+    totalPoints: Number,
+    overalGrade: String,
+    classTecher: { type: mongoose.Schema.ObjectId, ref: "classTeacher" },
+    classTeacherRemarks: { type: mongoose.Schema.ObjectId, ref: "classTeacherRemarks" },
+    principal: { type: mongoose.Schema.ObjectId, ref: "Principal" },
+    principalRemarks: { type: mongoose.Schema.ObjectId, ref: "PrincipalRemarks" },
+    isArchived: { type: Boolean, Default: false },
+    isActive: { type: Boolean, Default: true },
 });
 module.exports = mongoose.model("ExamLine", examLineSchema);
